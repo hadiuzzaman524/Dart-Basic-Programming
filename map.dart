8a354de761cca2ba29953cdf7ad5c6b3b17ff4f3
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2020-11-02 08:44:21 +0600

    more about list and map combinations

--- map.dart
+++ map.dart
@@ -1,5 +1,5 @@
 void main(){
-
+/*
   var gif={
     'jaman':'mantasha',
     'oshin':'Riaj',
@@ -7,6 +7,21 @@ void main(){
   };
   print(gif['hadi']);
   gif['print']='hadi';
-  print(gif['print']);
+  print(gif['print']);*/
+
+
+var questions=[
+  {'question':'What is your name?'},
+  {'answer':['jaman','habib','rakib']},
+];
+
+/*
+question[1] indicate the list 1 nb index, then ['answer'] track the list
+then i declare it clearly as List then map every element of list and access
+it. finally call toList function
+ */
+(questions[1]['answer'] as List<String>).map((e) {
+  print(e);
+}).toList();
 
 }
